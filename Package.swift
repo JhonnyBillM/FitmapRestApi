23b2372fe525eeb80a39520341e13a578e15254f
Rafael Suazo <rafaelsuazoh@gmail.com>
2017-01-15 23:09:30 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-01-16 03:03:40 +0000
+import PackageDescription
+let package = Package(
+    name: "FitmapRestApi",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 1),
+    ]
+)
